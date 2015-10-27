4101a183d7a99cb2fabb7ce1017974d98986fda0
mykemouuuse <mary_kyle_labarda@dlsu.edu.ph>
2015-10-27 22:28:16 +0800

    meow

---
+++ Frame.java
@@ -0,0 +1,128 @@
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Frame extends JFrame implements ActionListener{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	JPanel p = new JPanel();
+	JButton button[][] =  new JButton[3][3];
+	Game ttt;
+	public Frame(){
+		super("TicTacToe");
+		setSize(600,600);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		p.setLayout(new GridLayout(3,3));
+		for(int i=0 ; i<3 ; i++){
+			for(int j=0; j<3; j++){
+				button[i][j] = new JButton();
+				button[i][j].addActionListener(this);
+				button[i][j].setFont(new Font("Arial", Font.BOLD, 90));
+				p.add(button[i][j]);
+			}
+
+		}
+		add(p);
+		setVisible(true);
+
+		ttt = new Game();
+	}
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		if(event.getSource() == button[0][0]){
+			button[0][0].setText(Character.toString(ttt.setBoard(0, 0)));
+			if(ttt.GameOver() && ttt.draw != 0)
+				JOptionPane.showMessageDialog(this,"Game Over! Player: " + ttt.getTurn() + " Wins!!");
+			else if(ttt.GameOver() && ttt.draw==1)
+				JOptionPane.showMessageDialog(this,"It's a draw!");
+			else
+				ttt.turnSwitch();
+		}
+		if(event.getSource() == button[0][1]){
+			button[0][1].setText(Character.toString(ttt.setBoard(0, 1)));
+			if(ttt.GameOver()&& ttt.draw != 0)
+				JOptionPane.showMessageDialog(this,"Game Over! Player: " + ttt.getTurn() + " Wins!!");
+			else if(ttt.GameOver() && ttt.draw == 1)
+				JOptionPane.showMessageDialog(this,"It's a draw!");
+			else
+				ttt.turnSwitch();
+		}
+		if(event.getSource() == button[0][2]){
+			button[0][2].setText(Character.toString(ttt.setBoard(0, 2)));
+			if(ttt.GameOver() && ttt.draw != 0)
+				JOptionPane.showMessageDialog(this,"Game Over! Player: " + ttt.getTurn() + " Wins!!");
+			else if(ttt.GameOver() && ttt.draw==1)
+				JOptionPane.showMessageDialog(this,"It's a draw!");
+			else
+				ttt.turnSwitch();
+		}
+		if(event.getSource() == button[1][0]){
+			button[1][0].setText(Character.toString(ttt.setBoard(1, 0)));
+			if(ttt.GameOver() && ttt.draw != 0)
+				JOptionPane.showMessageDialog(this,"Game Over! Player: " + ttt.getTurn() + " Wins!!");
+			else if(ttt.GameOver() && ttt.draw==1)
+				JOptionPane.showMessageDialog(this,"It's a draw!");
+			else
+				ttt.turnSwitch();
+		}
+		if(event.getSource() == button[1][1]){
+			button[1][1].setText(Character.toString(ttt.setBoard(1, 1)));
+			if(ttt.GameOver() && ttt.draw != 0)
+				JOptionPane.showMessageDialog(this,"Game Over! Player: " + ttt.getTurn() + " Wins!!");
+			else if(ttt.GameOver() && ttt.draw==1)
+				JOptionPane.showMessageDialog(this,"It's a draw!");
+			else
+				ttt.turnSwitch();
+		}
+		if(event.getSource() == button[1][2]){
+			button[1][2].setText(Character.toString(ttt.setBoard(1, 2)));
+			if(ttt.GameOver() && ttt.draw != 0)
+				JOptionPane.showMessageDialog(this,"Game Over! Player: " + ttt.getTurn() + " Wins!!");
+			else if(ttt.GameOver() && ttt.draw==1)
+				JOptionPane.showMessageDialog(this,"It's a draw!");
+			else
+				ttt.turnSwitch();
+		}
+		if(event.getSource() == button[2][0]){
+			button[2][0].setText(Character.toString(ttt.setBoard(2, 0)));
+			if(ttt.GameOver() && ttt.draw != 0)
+				JOptionPane.showMessageDialog(this,"Game Over! Player: " + ttt.getTurn() + " Wins!!");
+			else if(ttt.GameOver() && ttt.draw==1)
+				JOptionPane.showMessageDialog(this,"It's a draw!");
+			else
+				ttt.turnSwitch();
+		}
+		if(event.getSource() == button[2][1]){
+			button[2][1].setText(Character.toString(ttt.setBoard(2, 1)));
+			if(ttt.GameOver() && ttt.draw != 0)
+				JOptionPane.showMessageDialog(this,"Game Over! Player: " + ttt.getTurn() + " Wins!!");
+				else if(ttt.GameOver() && ttt.draw==1)
+					JOptionPane.showMessageDialog(this,"It's a draw!");
+			else
+				ttt.turnSwitch();
+		}
+		if(event.getSource() == button[2][2]){
+			button[2][2].setText(Character.toString(ttt.setBoard(2, 2)));
+			if(ttt.GameOver()&& ttt.draw != 0)
+				JOptionPane.showMessageDialog(this,"Game Over! Player: " + ttt.getTurn() + " Wins!!");
+				else if(ttt.GameOver() && ttt.draw==1)
+					JOptionPane.showMessageDialog(this,"It's a draw!");
+			else
+				ttt.turnSwitch();
+		}
+	}
+
+
+
+
+}
---
+++ Game.java
@@ -0,0 +1,114 @@
+import java.awt.peer.FramePeer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+
+public class Game {
+
+	public static int row, col ;
+	private boolean playing;
+	public char[][] board = new char[3][3];
+	Scanner scan = new Scanner(System.in);
+	public char turn = 'X';
+	public int movecounter = 1;
+	public int draw = 0;
+
+
+
+	public Game(){
+		for (int i =0 ; i <3 ; i++)
+			for(int j =0; j < 3 ;j++)
+				board[i][j] = '_';
+
+		printBoard();
+
+	}
+
+	public void printBoard(){
+		for (int i =0 ; i <3 ; i++){
+			System.out.println( );
+			for(int j =0; j < 3 ;j++)
+				System.out.print(board[i][j] + "|");
+		}
+
+		System.out.println( );
+
+	}
+
+	public void play(){
+		playing = true;
+		while (playing){
+			System.out.println("Please enter row and column:");
+			row = scan.nextInt() - 1;
+			col = scan.nextInt() - 1;
+			if((row < 3 && row > -1) && (col < 3 && row > -1) && board[row][col] == '_'){
+			board[row][col] = turn;
+
+			if(GameOver()){
+				if(draw == 0){
+				playing = false;
+				System.out.println("player " + turn + " wins");
+			}
+			else{
+				playing = false;
+				System.out.println("It's a draw!");
+			}
+			}
+			printBoard();
+
+			if(turn == 'X')
+				turn = 'O';
+			else
+				turn = 'X';
+		}
+
+		else
+			System.out.println("Only input from 1-3/Aleady taken");
+		}
+		}
+
+	public boolean GameOver(){
+
+		for(int i = 0 ; i < 3; i++)
+		if(board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] != '_' )
+		return true;
+
+		for(int i = 0 ; i < 3; i++)
+	    if(board[0][i] == board[1][i] && board[0][i] == board[2][i] && board[0][i] != '_')
+		return true;
+
+		 if(board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != '_' )
+			return true;
+		 else if (board[2][0] == board[1][1] && board[2][0] == board[0][2] && board[2][0] != '_' )
+			return true;
+
+		if(movecounter == 9){
+			draw = 1;
+			return true;
+		}
+
+		 return false;
+	}
+
+	public void turnSwitch(){
+		if(turn == 'O')
+			turn = 'X';
+		else
+			turn = 'O';
+
+		movecounter++;
+	}
+
+	public char setBoard(int row, int col){
+		board[row][col] = turn;
+		return turn;
+	}
+	public char getTurn(){
+		return turn;
+	}
+
+
+}
